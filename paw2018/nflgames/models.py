6500d6a063b4fa13314c0e157eb41ccecca5c475
mdcollins80 <mdcollins80@gmail.com>
2018-08-30 15:04:01 -0400

    Adds weekday property to nflgames

--- paw2018/nflgames/models.py
+++ paw2018/nflgames/models.py
@@ -8,14 +8,21 @@ class Game(models.Model):
     home_team = models.ForeignKey(Team,
                                   on_delete=models.CASCADE,
                                   related_name="home_games")
-    home_team_score = models.SmallIntegerField()
+    home_team_score = models.SmallIntegerField(blank=True,
+                                               null=True)
     away_team = models.ForeignKey(Team,
                                   on_delete=models.CASCADE,
                                   related_name="away_games")
-    away_team_score = models.SmallIntegerField()
+    away_team_score = models.SmallIntegerField(blank=True,
+                                               null=True)
 
     def __str__(self):
-        return self.home_team + '@ ' + self.away_team
+        return self.away_team.name + ' @ ' + self.home_team.name
+
+    @property
+    def weekday(self):
+        date = self.kickoff
+        return date.strftime("%A")
 
     @property
     def season(self):
