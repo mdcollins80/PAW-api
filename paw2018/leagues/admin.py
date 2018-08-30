6a062b2153b0af510749ad2677a7e386db79c63b
mdcollins80 <mdcollins80@gmail.com>
2018-08-30 11:06:53 -0400

    Adds League model and admin

--- paw2018/leagues/admin.py
+++ paw2018/leagues/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import League
 
 # Register your models here.
+
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'league_name', 'league_picture',]
+
+admin.site.register(League)
--- paw2018/leagues/models.py
+++ paw2018/leagues/models.py
@@ -6,3 +6,6 @@ class League(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='leagues')
     league_name = models.CharField(max_length=255)
     league_picture = models.ImageField(blank=True)
+
+    def __str__(self):
+        return self.league_name
