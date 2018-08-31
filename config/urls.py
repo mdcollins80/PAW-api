9810f2150843d8cecfcf240f86c02cb44ecccadf
mdcollins80 <mdcollins80@gmail.com>
2018-08-30 21:28:28 -0400

    Sets up users api endpoint

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,16 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from rest_framework.routers import DefaultRouter
+
+from paw2018.users.urls import api_router as user_router
+
+class MasterRouter(DefaultRouter):
+    def register_subrouter(self, subrouter):
+        self.registry.extend(subrouter.registry)
+
+api_router = MasterRouter()
+api_router.register_subrouter(user_router)
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -21,6 +31,10 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+
+    #API stuff
+    path("api/", include(api_router.urls)),
+    path("api-auth/", include('rest_framework.urls', namespace='rest_framework')),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
--- paw2018/users/urls.py
+++ paw2018/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from .views import UserViewSet
 
 from paw2018.users.views import (
     user_list_view,
@@ -7,8 +10,12 @@ from paw2018.users.views import (
     user_detail_view,
 )
 
+api_router = routers.DefaultRouter()
+api_router.register(r'users', UserViewSet)
+
 app_name = "users"
 urlpatterns = [
+    path("", include(api_router.urls)),
     path("", view=user_list_view, name="list"),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
