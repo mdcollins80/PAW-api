d000e701c8c670294b59c143dd0349d97acdce22
mdcollins80 <mdcollins80@gmail.com>
2018-09-14 19:12:34 -0400

    Creates new route for owned teams
    - enables me to show all teams on homepage and owned team on My Team
      page

--- paw2018/userteams/views.py
+++ paw2018/userteams/views.py
@@ -6,3 +6,12 @@ from .models import UserTeam
 class UserTeamViewSet(viewsets.ModelViewSet):
     queryset = UserTeam.objects.all()
     serializer_class = UserTeamSerializer
+
+
+class UserTeamOwned(viewsets.ModelViewSet):
+    serializer_class = UserTeamSerializer
+
+    def get_queryset(self):
+        owner = self.request.user
+        queryset = UserTeam.objects.filter(owner=owner)
+        return queryset
