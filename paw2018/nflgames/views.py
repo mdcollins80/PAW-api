2b9bf9a48dbf9d611f552b96d47ee508b022476f
mdcollins80 <mdcollins80@gmail.com>
2018-09-18 17:34:46 -0400

    Adds prefetch teams to GameViewSet

--- paw2018/nflgames/views.py
+++ paw2018/nflgames/views.py
@@ -4,5 +4,5 @@ from .models import Game
 
 # Create your views here.
 class GameViewSet(viewsets.ModelViewSet):
-    queryset = Game.objects.all()
+    queryset = Game.objects.all().order_by('id').prefetch_related('home_team', 'away_team')
     serializer_class = GameSerializer
