06a16f2439ecab1914b362a3cfc66dbb04f7cb4f
mdcollins80 <mdcollins80@gmail.com>
2018-09-20 16:02:28 -0400

    Adds user owned route for userpicks

--- paw2018/userpicks/urls.py
+++ paw2018/userpicks/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import UserPickViewSet, UserPickPostSet
+from .views import UserPickViewSet, UserPickPostSet, UserPickOwned
 
 api_router = routers.DefaultRouter()
 api_router.register(r'userpicks', UserPickViewSet)
 api_router.register(r'userpicksPost', UserPickPostSet, 'userpickPost')
+api_router.register(r'userpicksOwned', UserPickOwned, 'userpickOwned')
 
 app_name = 'userpicks'
 urlspatterns = [
--- paw2018/userpicks/views.py
+++ paw2018/userpicks/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .serializers import UserPickSerializer, UserPickPostSerializer
 from .models import UserPick
+from paw2018.userteams.models import UserTeam
 
 # Create your views here.
 class UserPickViewSet(viewsets.ModelViewSet):
@@ -9,4 +10,15 @@ class UserPickViewSet(viewsets.ModelViewSet):
 
 
 class UserPickPostSet(viewsets.ModelViewSet):
+    queryset = UserPick.objects.all().prefetch_related('pick','game','team')
     serializer_class = UserPickPostSerializer
+
+
+class UserPickOwned(viewsets.ModelViewSet):
+    serializer_class = UserPickSerializer
+
+    def get_queryset(self):
+        owner = self.request.user
+        team = UserTeam.objects.get(owner=owner)
+        queryset = UserPick.objects.filter(team=team)
+        return queryset
